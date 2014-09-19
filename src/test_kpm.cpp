259ea4ece6156740488f045ba87aa8089e03fe10
Kipton Barros <kbarros@gmail.com>
2014-09-18 21:57:19 -0600

    Refactor KPM

--- src/test_kpm.cpp
+++ src/test_kpm.cpp
@@ -16,10 +16,10 @@ namespace tibidy {
     
     // TODO: move to fastkpm
     double exactEnergy(arma::sp_cx_mat H, double kB_T, double mu) {
-        auto eigs = arma::eig_gen(sparseToDense(H));
+        auto eigs = arma::eig_gen(sparse_to_dense(H));
         double ret = 0.0;
         for (auto x : eigs) {
-            ret += fermiEnergy(x.real(), kB_T, mu);
+            ret += fermi_energy(x.real(), kB_T, mu);
         }
         return ret;
     }
@@ -30,7 +30,7 @@ namespace tibidy {
         EnergyScale es { -1, +1 };
         for (int Mq : Mqs) {
             auto f = [](double e) { return e < 0.12 ? e : 0; };
-            auto c = expansionCoefficients(M, Mq, f, es);
+            auto c = expansion_coefficients(M, Mq, f, es);
             std::cout << "quad. points=" << Mq << ", c=" << c << std::endl;
             Vec<double> c_exact { -0.31601, 0.4801, -0.185462, -0.000775683, 0.0255405, 0.000683006,
                 -0.00536911, -0.000313801, 0.000758494, 0.0000425209 };
@@ -48,21 +48,21 @@ namespace tibidy {
         
         double extra = 0.1;
         double tolerance = 1e-2;
-        auto es = energyScale(H, extra, tolerance);
-        auto engine = mkEngineCx(n, n);
-        engine->setHamiltonian(H, es);
-        allVectorsCx(engine->R);
+        auto es = energy_scale(H, extra, tolerance);
+        auto engine = mk_engine_cx(n, n);
+        engine->set_H(H, es);
+        engine->set_R_identity();
         
         int M = 1000;
         int Mq = 4*M;
-        auto g_c = expansionCoefficients(M, Mq, g, es);
-        auto f_c = expansionCoefficients(M, Mq, f, es);
+        auto g_c = expansion_coefficients(M, Mq, g, es);
+        auto f_c = expansion_coefficients(M, Mq, f, es);
         
         double E1 = engine->trace(g_c);
         cout << "energy (v1) " << E1 << " expected 50.0004 for M=1000\n";
         
-        auto gamma = momentTransform(engine->moments(M), Mq);
-        double E2 = densityProduct(gamma, g, es);
+        auto gamma = moment_transform(engine->moments(M), Mq);
+        double E2 = density_product(gamma, g, es);
         cout << "energy (v2) " << E2 << endl;
         
         double E3 = engine->trace(g_c, arma::speye<arma::sp_cx_mat>(n, n));
@@ -80,11 +80,12 @@ namespace tibidy {
         double noise = 0.2;
         RNG rng;
         rng.seed(0);
+        std::normal_distribution<double> normal;
         
         // Build noisy tri-diagonal matrix
         MatrixBuilder<arma::cx_double> mb;
         for (int i = 0; i < n; i++) {
-            auto x = 1.0 + noise * randomNormalCx(rng);
+            auto x = 1.0 + noise * arma::cx_double(normal(rng), normal(rng));
             int j = (i-1+n)%n;
             mb.add(i, j, x);
             mb.add(j, i, conj(x));
@@ -94,20 +95,20 @@ namespace tibidy {
         double mu = 0.2;
         
         using std::placeholders::_1;
-        auto g = std::bind(fermiEnergy, _1, kB_T, mu);
-        auto f = std::bind(fermiDensity, _1, kB_T, mu);
+        auto g = std::bind(fermi_energy, _1, kB_T, mu);
+        auto f = std::bind(fermi_density, _1, kB_T, mu);
         
         double extra = 0.1;
         double tolerance = 1e-2;
-        auto es = energyScale(H, extra, tolerance);
+        auto es = energy_scale(H, extra, tolerance);
         int M = 2000;
         int Mq = 4*M;
-        auto g_c = expansionCoefficients(M, Mq, g, es);
-        auto f_c = expansionCoefficients(M, Mq, f, es);
-        auto engine1 = mkEngineCx(n, n);
-        auto engine2 = mkEngineCx(n, n/4);
-        engine1->setHamiltonian(H, es);
-        engine2->setHamiltonian(H, es);
+        auto g_c = expansion_coefficients(M, Mq, g, es);
+        auto f_c = expansion_coefficients(M, Mq, f, es);
+        auto engine1 = mk_engine_cx(n, n);
+        auto engine2 = mk_engine_cx(n, n/4);
+        engine1->set_H(H, es);
+        engine2->set_H(H, es);
         
         double E1 = exactEnergy(H, kB_T, mu);
         double eps = 1e-6;
@@ -117,18 +118,18 @@ namespace tibidy {
         dH(j, i) = eps;
         auto dE_dH_1 = (exactEnergy(H+dH, kB_T, mu)-exactEnergy(H-dH, kB_T, mu)) / (2*eps);
         
-        allVectorsCx(engine1->R);
+        engine1->set_R_identity();
         double E2 = engine1->trace(g_c);
         auto dE_dH_2 = engine1->deriv(f_c);
         
-        uncorrelatedVectorsCx(rng, engine2->R);
+        engine2->set_R_uncorrelated(rng);
         double E3 = engine2->trace(g_c);
         auto dE_dH_3 = engine2->deriv(f_c);
         
         Vec<int> grouping(n);
         for (int i = 0; i < n; i++)
             grouping[i] = i%engine2->s;
-        correlatedVectorsCx(grouping, rng, engine2->R);
+        engine2->set_R_correlated(grouping, rng);
         double E4 = engine2->trace(g_c);
         auto dE_dH_4 = engine2->deriv(f_c);
         

688882bc37b278d2973a599190e321b564d304a2
小澤 遼 (Ryo Ozawa) <ozawa@aion.t.u-tokyo.ac.jp>
2014-11-19 08:12:04 +0900

    some minor changes

--- src/merons.cpp
+++ src/merons.cpp
@@ -4,21 +4,21 @@
 
 int main(int argc,char **argv) {
     RNG rng(0);
-    int w = 16;
+    int w = 8;
     double t1 = -1, t2 = 0, t3 = 0;
     double kT = 0;
-    int n_colors = 4;
+    int n_colors = 16;
     int M = 1000;
     int Mq = 4*M;
     EnergyScale es{-10, 10};
     
     double min_J = 0;
-    double max_J = 5 + 1e-4;
-    double d_J = 1; // 0.1;
+    double max_J = 1 + 1e-4;
+    double d_J = 0.1; // 0.1;
     
-    double min_mu = -5;
-    double max_mu = +5 + 1e-4;
-    double d_mu = 2; // 0.5;
+    double min_mu = -2.6;
+    double max_mu = -2.3 + 1e-4;
+    double d_mu = 0.05; // 0.5;
     
     auto m = Model(SquareLattice::mk(w, w, t1, t2, t3), min_J, kT);
     
@@ -39,7 +39,8 @@ int main(int argc,char **argv) {
         for (double mu = min_mu; mu < max_mu; mu += d_mu) {
             auto g = std::bind(fermi_energy, std::placeholders::_1, kT, mu);
             double Phi = density_product(gamma, g, es) / m.lattice->n_sites();
-            cout << m.J << " " << mu << " " << Phi << endl;
+            //cout << m.J << " " << mu << " " << Phi << endl;
+            printf("%10lf, %10lf, %10lf\n", m.J, mu, Phi);
         }
     }
 }

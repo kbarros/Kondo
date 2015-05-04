0a0c65d8f0c0ceb436d69a24f2eedf2370372d62
Kipton Barros <kbarros@gmail.com>
2015-05-04 16:04:39 -0600

    Make vec3 operations constexpr.

--- src/vec3.h
+++ src/vec3.h
@@ -13,49 +13,49 @@ class Vec3 {
 public:
     T x=0, y=0, z=0;
     
-    Vec3(T x, T y, T z): x(x), y(y), z(z) {}
+    constexpr Vec3(T x, T y, T z): x(x), y(y), z(z) {}
     
-    Vec3<T> operator-() const {
+    constexpr Vec3<T> operator-() const {
         return {-x, -y, -z};
     }
     
-    Vec3<T> operator+(Vec3<T> that) const {
+    constexpr Vec3<T> operator+(Vec3<T> that) const {
         return {x+that.x, y+that.y, z+that.z};
     }
 
-    Vec3<T> operator-(Vec3<T> that) const {
+    constexpr Vec3<T> operator-(Vec3<T> that) const {
         return {x-that.x, y-that.y, z-that.z};
     }
 
-    Vec3<T> operator*(T a) const {
+    constexpr Vec3<T> operator*(T a) const {
         return {x*a, y*a, z*a};
     }
     
-    friend Vec3<T> operator *(T x, Vec3<T> y) {
+    constexpr friend Vec3<T> operator *(T x, Vec3<T> y) {
         return y*x;
     }
     
-    Vec3<T> operator/(T a) const {
+    constexpr Vec3<T> operator/(T a) const {
         return {x/a, y/a, z/a};
     }
     
-    T dot(Vec3<T> const& that) const {
+    constexpr T dot(Vec3<T> const& that) const {
         return x*that.x + y*that.y + z*that.z;
     }
     
-    Vec3<T> cross(Vec3<T> const& that) const {
+    constexpr Vec3<T> cross(Vec3<T> const& that) const {
         return {y*that.z-z*that.y, z*that.x-x*that.z, x*that.y-y*that.x};
     }
     
-    T norm2() const {
+    constexpr T norm2() const {
         return dot(*this);
     }
     
-    T norm() const {
+    constexpr T norm() const {
         return sqrt(norm2());
     }
     
-    Vec3<T> normalized() const {
+    constexpr Vec3<T> normalized() const {
         return *this / norm();
     }
     
@@ -84,7 +84,7 @@ public:
     }
     
     template <typename S>
-    operator Vec3<S>() const {
+    constexpr operator Vec3<S>() const {
         return Vec3<S>(x, y, z);
     }
     
@@ -94,12 +94,12 @@ public:
 };
 
 template <typename S>
-Vec3<S> real(Vec3<std::complex<S>> v) {
+constexpr Vec3<S> real(Vec3<std::complex<S>> v) {
     return {std::real(v.x), std::real(v.y), std::real(v.z)};
 }
 
 template <typename S>
-Vec3<S> imag(Vec3<std::complex<S>> v) {
+constexpr Vec3<S> imag(Vec3<std::complex<S>> v) {
     return {std::imag(v.x), std::imag(v.y), std::imag(v.z)};
 }
 
